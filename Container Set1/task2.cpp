c5ac344d9885b265ff276823365df740cea9fae5
IrynaNazarchuk <165282189+IrynaNazarchuk@users.noreply.github.com>
2024-04-11 21:55:22 +0300

    Update task2.cpp

--- Container Set1/task2.cpp
+++ Container Set1/task2.cpp
@@ -1,25 +1,25 @@
 #include "Set.h"
 #include <iostream>
 
-// Функція для обчислення кількості різних цифр у числі
+// Function to calculate the number of different digits in a number
 int digits(int n) {
-    Set<int> digitSet; // Створюємо екземпляр класу Set для зберігання різних цифр
+    Set<int> digitSet; // Create an instance of the Set class to store different numbers
 
-    // Проходимося по кожній цифрі числа n
+  // We go through each digit of the number n
     while (n > 0) {
-        int digit = n % 10; // Отримуємо останню цифру числа
-        digitSet.add(digit); // Додаємо цифру до множини
-        n /= 10; // Видаляємо останню цифру числа
+        int digit = n % 10; // Get the last digit of the number
+        digitSet.add(digit); // Add a number to the set
+        n /= 10; // Delete the last digit of the number
     }
 
-    // Повертаємо кількість елементів у множині (кількість різних цифр)
+  // Return the number of elements in the set (the number of different digits)
     return digitSet.getSize();
 }
 
 int main() {
-    std::cout << "digits(2024) = " << digits(2024) << std::endl; // Очікуване значення: 3
-    std::cout << "digits(5) = " << digits(5) << std::endl; // Очікуване значення: 1
-    std::cout << "digits(10110111) = " << digits(10110111) << std::endl; // Очікуване значення: 2
+    std::cout << "digits(2024) = " << digits(2024) << std::endl; // Expected value: 3
+    std::cout << "digits(5) = " << digits(5) << std::endl; // Expected value: 1
+    std::cout << "digits(10110111) = " << digits(10110111) << std::endl; // Expected value: 2
 
     return 0;
 }
