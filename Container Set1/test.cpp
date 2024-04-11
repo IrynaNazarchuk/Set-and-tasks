c633d76392c0735fd10444380b65671e935450cd
IrynaNazarchuk <165282189+IrynaNazarchuk@users.noreply.github.com>
2024-04-11 12:19:31 +0300

    Add files via upload

---
+++ Container Set1/test.cpp
@@ -0,0 +1,66 @@
+#include "pch.h"
+#include <gtest/gtest.h>
+#include "Set.h"
+
+TEST(SetTest, AddAndContains) {
+    Set<int> set;
+    set.add(1);
+    set.add(2);
+    set.add(3);
+
+    EXPECT_TRUE(set.contains(1));
+    EXPECT_TRUE(set.contains(2));
+    EXPECT_TRUE(set.contains(3));
+    EXPECT_FALSE(set.contains(4));
+}
+
+TEST(SetTest, Remove) {
+    Set<int> set = { 1, 2, 3 };
+    set.remove(2);
+
+    EXPECT_TRUE(set.contains(1));
+    EXPECT_FALSE(set.contains(2));
+    EXPECT_TRUE(set.contains(3));
+}
+
+TEST(SetTest, Union) {
+    Set<int> set1 = { 1, 2, 3 };
+    Set<int> set2 = { 3, 4, 5 };
+    Set<int> expected = { 1, 2, 3, 4, 5 };
+
+    Set<int> unionSet = set1.unionWith(set2);
+    EXPECT_EQ(unionSet, expected);
+}
+
+TEST(SetTest, Intersection) {
+    Set<int> set1 = { 1, 2, 3 };
+    Set<int> set2 = { 3, 4, 5 };
+    Set<int> expected = { 3 };
+
+    Set<int> intersectionSet = set1.intersectionWith(set2);
+    EXPECT_EQ(intersectionSet, expected);
+}
+
+TEST(SetTest, Difference) {
+    Set<int> set1 = { 1, 2, 3 };
+    Set<int> set2 = { 3, 4, 5 };
+    Set<int> expected = { 1, 2 };
+
+    Set<int> differenceSet = set1.differenceWith(set2);
+    EXPECT_EQ(differenceSet, expected);
+}
+
+TEST(SetTest, AddSet) {
+    Set<int> set1 = { 1, 2, 3 };
+    Set<int> set2 = { 4, 5, 6 };
+    Set<int> expected = { 1, 2, 3, 4, 5, 6 };
+
+    set1.add(set2);
+    EXPECT_EQ(set1, expected);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
