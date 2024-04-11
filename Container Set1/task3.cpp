913c9e2e34093300fd480d2936cc0479edf5265b
IrynaNazarchuk <165282189+IrynaNazarchuk@users.noreply.github.com>
2024-04-11 21:58:43 +0300

    Update task3.cpp

--- Container Set1/task3.cpp
+++ Container Set1/task3.cpp
@@ -5,18 +5,18 @@
 int main() {
     std::string str = "abracadabra";
 
-    // Перше входження кожної літери
+    // First occurrence of each letter
     Set<char> firstOccurrences;
     for (char c : str) {
         firstOccurrences.add(c);
     }
-    std::cout << "Перше входження кожної літери: ";
+    std::cout << "The first occurrence of each letter: ";
     for (size_t i = 0; i < firstOccurrences.getSize(); ++i) {
         std::cout << firstOccurrences[i] << " ";
     }
     std::cout << std::endl;
 
-    // Літери, які зустрічаються не менше двох разів
+   // Letters that occur at least twice
     Set<char> multipleOccurrences;
     Set<char> uniqueChars;
     for (char c : str) {
@@ -27,15 +27,15 @@ int main() {
             uniqueChars.add(c);
         }
     }
-    std::cout << "Літери, які зустрічаються не менше двох разів: ";
+    std::cout << "Letters that occur at least twice: ";
     for (size_t i = 0; i < multipleOccurrences.getSize(); ++i) {
         std::cout << multipleOccurrences[i] << " ";
     }
     std::cout << std::endl;
 
-    // Літери, які зустрічаються тільки один раз
+    // Letters that occur only once
     Set<char> singleOccurrences = uniqueChars.differenceWith(multipleOccurrences);
-    std::cout << "Літери, які зустрічаються тільки один раз: ";
+    std::cout << "Letters that occur only once: ";
     for (size_t i = 0; i < singleOccurrences.getSize(); ++i) {
         std::cout << singleOccurrences[i] << " ";
     }
